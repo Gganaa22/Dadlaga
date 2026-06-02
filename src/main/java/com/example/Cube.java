ab9b597e768396d50c7f8faa2721169bcb058eeb
Gganaa22 <darkhanbaatargantogtokh47@email.com>
2026-06-02 12:06:58 +0800

    first commit

---
+++ src/main/java/com/example/Cube.java
@@ -0,0 +1,15 @@
+package com.example;
+
+class Cube {
+     public void calculate() {
+
+        double a = 10.1;
+
+        double s = a * a * 6;
+        System.out.println("Kubiin talbai: " + s);
+
+        double v = a * a * a;
+        System.out.println("Kubiin ezelhuun: " + v);
+    }
+    
+}
